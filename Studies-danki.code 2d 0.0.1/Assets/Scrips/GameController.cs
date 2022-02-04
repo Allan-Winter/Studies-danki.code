adc03401f4146f82ebe538adb93feb69ae38139b
Allan-Winter <alan.souza8000@gmail.com>
2022-02-04 11:01:41 -0300

    Studies-danki.code

--- Studies-danki.code 2d 0.0.1/Assets/Scrips/GameController.cs
+++ Studies-danki.code 2d 0.0.1/Assets/Scrips/GameController.cs
@@ -8,12 +8,18 @@ public class GameController : MonoBehaviour
     public Text scoreText;
 
     public int score;
+    public int totalScore;
 
     public static GameController instance;
 
     void Awake()
     {
         instance = this;
+    }
+
+    private void Start()
+    {
+        totalScore = PlayerPrefs.GetInt("score");
     }
 
     // Update is called once per frame
@@ -26,6 +32,8 @@ public class GameController : MonoBehaviour
     {
         score += value;
         scoreText.text = "X " + score.ToString();
+
+        PlayerPrefs.SetInt("score", score + totalScore);
     }
 
     public void UpdateLives(int value)
